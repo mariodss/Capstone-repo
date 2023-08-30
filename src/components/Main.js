ef9f3a3039d1c6646f867e9e357e45a7025fdbee
mariodss <127170381+mariodss@users.noreply.github.com>
2023-08-30 12:09:05 -0400

    new update

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,69 @@
+import React from 'react';
+import { useReducer } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import Header from './Header';
+import Booking from './booking';
+
+const Main = () => {
+
+    const seedRandom = function (seed) {
+        var m = 2 ** 35 - 31;
+        var a = 185852;
+        var s = seed % m;
+        return function () {
+
+            return (s = s * a % m) / m;
+
+        }
+
+    }
+
+    const fetchAPI = function (date) {
+
+        let result = [];
+        let random = seedRandom(date.getDate());
+        for (let i = 17; i <= 23; i++) {
+            if (random() < 0.5) {
+                result.push(i + ':00');
+
+            }
+            if (random() > 0.5) {
+                result.push(i + ':30');
+
+            }
+        }
+        return result;
+
+    }
+
+    const initialState = { availableTimes: fetchAPI(new Date()) };
+    const [state, dispatch] = useReducer(updateTimes, initialState);
+
+    function updateTimes(state, date) {
+        return { availableTimes: fetchAPI(new Date()) }
+
+    }
+
+    const navigate = useNavigate();
+    function submitForm(formData) {
+        if (submitAPI(formData)) {
+            navigate("/confirmed");
+        }
+
+    }
 
 
-
-function Main() {
 
     return (
         <main>
+            <Routes>
+                <Route path='/' element={<Header />} />
+                <Route path='/boooking' element={<Booking availableTimes={state} dispatch={dispatch}
+                    submitForm={submitForm} />} />
+                <Route path='/' element={<Header />} />
+
+            </Routes>
+
 
         </main>
 
